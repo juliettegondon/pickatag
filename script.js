bcf6e495024c79fdb753035df2124d9cb9c32e1e
Juliette Gondon <juliettegondon@gmail.com>
2020-11-21 22:39:12 -0500

    added styling

--- script.js
+++ script.js
@@ -16,56 +16,56 @@ input.addEventListener('keyup', (e) => {
 	}
 });
 
+// create buttons using user input entered in the above loop, clean them up and convert to innerhtml
+//append buttons to the parent container 
 function createButtons(input) {
 	const buttons = input.split(',').filter(button => button.trim() !== '').map(button => button.trim());
-	
-	// clean up the buttons first
-	buttonsEl.innerHTML = '';
-	
-	// map over the buttons and add them to the buttonsEl container
+    buttonsEl.innerHTML = '';
+    console.log("BUTTON TEXT ---->", buttons)
 	buttons.forEach(button => {
-		const buttonEl = document.createElement('span');
+        const buttonEl = document.createElement('span');
 		buttonEl.classList.add('button');
 		buttonEl.innerText = button;
-		buttonsEl.appendChild(buttonEl);
+        buttonsEl.appendChild(buttonEl);
+        
 	})
 }
 
+// function to randomly select a button from the class list
+// set timeout allows 3000 milliseconds to go by while highlighting each button from list
+// unhighlight button removes the class of highlight
 function randomSelector() {
 let times = 30;
 let interval = setInterval(() => {
 		const randomButton = pickRandomButton();
-		
 		highlightButton(randomButton);
 		
-		// remove the highlight after a while
 		setTimeout(() => {
 			unhighlightButton(randomButton);
 		}, 100);
-	}, 100);
-	
-	// allow times * 100 ms for the buttons to randomly "highlight" themselves
-	// then pick another button
+    }, 100);
+   
+
 	setTimeout(() => {
 		clearInterval(interval);
-		
 		setTimeout(() => {
 		let randomButton = pickRandomButton();
-			
 			highlightButton(randomButton)
 		}, 100);
-	}, times * 100);
+    }, times * 100);
 }
 
+// using math.floor and math.random, random button from array will be selected
+// selected button will get css class highlight applied to it
 function pickRandomButton() {
-	const buttons = document.querySelectorAll('.button');
+	let buttons = document.querySelectorAll('.button');
 	return buttons[Math.floor(Math.random() * buttons.length)];
 }
 
 function highlightButton(button) {
-	button.classList.add('highlight');
+	button.classList.add('chosen');
 }
 
 function unhighlightButton(button) {
-	button.classList.remove('highlight');
+	button.classList.remove('chosen');
 }
